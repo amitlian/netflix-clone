fed65c7f08185ab22d74f1014dc1c1e362dfe9a1
Amit <lianbadhon@gmail.com>
2021-02-08 21:32:00 +0600

    first commit

---
+++ src/HomescreenFolder/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "../HomescreenFolder/HomeScreen.css"
+
+function HomeScreen(){
+    return(
+        <div className="homescreen">
+            {/* Nav */}
+            {/* Banner */}
+            {/* Row */}
+        </div>
+    )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCNsO1Md6GqmF-CdN8JSMcfV8FZnCtiqGc",
+    authDomain: "netflix-clone-made-by-amit.firebaseapp.com",
+    projectId: "netflix-clone-made-by-amit",
+    storageBucket: "netflix-clone-made-by-amit.appspot.com",
+    messagingSenderId: "323132898671",
+    appId: "1:323132898671:web:219bff0421953f0f16bc9a"
+  };
