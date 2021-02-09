9ce4149874d0262a305dc6db6519f5567d3aad6e
Amit <lianbadhon@gmail.com>
2021-02-09 10:58:22 +0600

    updating

--- src/BannerFolder/Banner.js
+++ src/BannerFolder/Banner.js
@@ -1,7 +1,28 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "../BannerFolder/Banner.css"
+import axios from "../axios"
+import requests from "../requests"
 
 function Banner() {
+
+    const [movie,setMovie] = useState([]);
+
+    useEffect(()=>{
+        async function fetchData() {
+            const request = await axios.get(requests.fetchActionMovies);
+            console.log(request.data.results);
+            setMovie(
+                request.data.results[Math.floor(Math.random()*request.data.results.length - 1)]
+            );
+            return request;     
+        }
+
+        fetchData();
+
+    },[requests.fetchActionMovies])
+    console.log(movie);
+
+
     function truncate(string,n) {
         return string?.length >n? string.substr(0,n-1) + '....': string;
 
@@ -9,16 +30,18 @@ function Banner() {
     }
     return(
         <header className="banner" style={{
-            backgroundImage: `url("https://xeraone.com/wp-content/uploads/Daredevil-Banner-1024x474.png")`,
+            // https  to original this need to fetch image from netflix
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,  
             backgroundSize: "cover",
-            backgroundPosition: "center center",
+            backgroundPosition: "center",
+            margin: "auto auto"
         }}>
             <div className="banner_content">
-                <h1 className="banner_title">Movie Name</h1>
+                <h1 className="banner_title">{movie?.original_title || movie?.title || movie?.name }</h1>
                 <div className="banner_buttons">
                     <button className="banner_button">Play</button>
                     <button className="banner_button">My List</button>
-                <h1 className="banner_discription">{truncate('Marvels Daredevil, or simply Daredevil, is an American television series created by Drew Goddard for the streaming service Netflix, based on the Marvel Comics character of the same name. It is set in the Marvel Cinematic Universe (MCU), sharing continuity with the films of the franchise, and is the first in a series of shows that lead to The Defenders crossover miniseries. The series is produced by Marvel Television in association with ABC Studios, with DeKnight Productions for the first season and Goddard Textiles for the first and second seasons. Steven S. DeKnight served as showrunner on the first season, with Doug Petrie and Marco Ramirez taking over for the second season, and Erik Oleson joining the series as its showrunner for its third season; Goddard served as a consultant for the series.',150)}</h1>
+                <h1 className="banner_discription">{truncate(movie?.overview,150)}</h1>
                 </div>
             </div>
 
